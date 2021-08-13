edf55cbabda50722c09882dbf3e79b7d05e32500
oneweek.lee <oneweek.lee@navercorp.com>
2021-08-13 18:08:20 +0900

    [#1] FE 기본 디렉토리 설정

---
+++ client/src/index.tsx
@@ -0,0 +1,8 @@
+import React, {FC} from 'react'
+import ReactDOM from 'react-dom'
+
+const App: FC = () => {
+    return <div>sample</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
