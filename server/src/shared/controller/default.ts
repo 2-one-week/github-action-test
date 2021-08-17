66d0afef2de47f9e08497e5ce86f87f76c8ed2fc
oneweek-lee <oneweek.lee@naver.com>
2021-08-17 11:19:21 +0900

    [#1] default controller (router) 추가

---
+++ server/src/shared/controller/default.ts
@@ -0,0 +1,12 @@
+import Router from 'koa-router'
+
+class DefaultRouter extends Router {
+    constructor() {
+        super()
+        this.initRouter()
+    }
+
+    initRouter() {}
+}
+
+export default DefaultRouter
